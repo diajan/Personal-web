1d95aaf5bd9560f40002d6e71906354cb20e6790
Mahdi Aghajan <aghajan2828@gmail.com>
2021-11-06 22:32:04 +0330

    add changes on index page

--- src/components/utils/BurgerMenu.tsx
+++ src/components/utils/BurgerMenu.tsx
@@ -9,9 +9,9 @@ const BurgerMenu: React.FC<{}> = () => {
         viewBox='0 0 20 20'
         fill='currentColor'>
         <path
-          fill-rule='evenodd'
+          fillRule='evenodd'
           d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-          clip-rule='evenodd'
+          clipRule='evenodd'
         />
       </svg>
     </div>
